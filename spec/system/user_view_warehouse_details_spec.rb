7767dd578d494d1a0299a777c2e9334107f821f3
Mariana S <marianaletii@gmail.com>
2022-09-16 22:15:54 -0300

    Adicionando estado à cidade

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Usuario visita página de detalhes' do
     it 'e vê informações adicionais' do
         #Arrange
-        Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+        Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos, SP', area: 100_000,
                          address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
                          description: 'Galpão destinado para cargas internacionais')
         #Act
@@ -13,7 +13,7 @@ describe 'Usuario visita página de detalhes' do
 
         expect(page).to have_content('Galpão GRU')
         expect(page).to have_content('Nome: Aeroporto SP')
-        expect(page).to have_content('Cidade: Guarulhos')
+        expect(page).to have_content('Cidade: Guarulhos, SP')
         expect(page).to have_content('Área: 100000 m2')
         expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
         expect(page).to have_content('Galpão destinado para cargas internacionais')
