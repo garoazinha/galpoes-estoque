fbef4671f8ec8ede6a5103ac854e64e57ecbd701
Mariana S <marianaletii@gmail.com>
2022-09-18 22:41:39 -0300

    Formulário de cadastro

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,15 @@ class WarehousesController < ApplicationController
     def show
         @warehouse = Warehouse.find(params[:id])
     end
+    def new
+        
+    end
+    def create
+        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :state,
+                                                            :address, :cep, :area,
+                                                            :useful_area, :description)
+        w = Warehouse.new(warehouse_params)
+        w.save()
+        redirect_to root_path
+    end
 end
