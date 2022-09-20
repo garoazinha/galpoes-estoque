15f02d90024b07643c5d5bb778fe5455e72e2445
Mariana S <marianaletii@gmail.com>
2022-09-19 21:17:02 -0300

    Adicionando mensagens de validação, ainda não traduzidas

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,6 +2,6 @@ class Warehouse < ApplicationRecord
     
     validates :name, :city, :code, :area, :state, :useful_area, :description, :cep, :address, presence: true
     validates :code, uniqueness:true
-    validates :cep, length: { is: 9 }
-    validates :cep, format: { with: /[0-9]{5}-[0-9]{3}/  }
+   
+    validates :cep, format: { with: /\A[0-9]{5}-[0-9]{3}\z/, message: 'em formato errado (00000-000)' }
 end
