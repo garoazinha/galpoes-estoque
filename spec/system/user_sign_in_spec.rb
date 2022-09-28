02acd81fb3b77cf82be5bacdf9419f15570633d2
Mariana S <marianaletii@gmail.com>
2022-09-27 21:33:40 -0300

    Login e Logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -15,12 +15,35 @@ describe 'Usuário se autentica' do
     end
 
     #Assert
-    expect(page).to have_link('Sair')
+    expect(page).to have_button('Sair')
     expect(page).not_to have_link('Entrar')
 
     within('nav') do
       expect(page).to have_content('mari@mari.com')
     end
     expect(page).to have_content('Login efetuado com sucesso')
+  end
+
+  it 'e faz logout' do
+    #Arrange
+    User.create!(name: 'Mariana', email:'mari@mari.com', password: 'password')
+    #Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'mari@mari.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    #Assert 
+    expect(page).to have_content('Logout efetuado com sucesso')
+    expect(page).to have_link('Entrar')
+    expect(page).not_to have_button('Sair')
+    expect(page).not_to have_content('mari@mari.com')
+
+
+
   end
 end
