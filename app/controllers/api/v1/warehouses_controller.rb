7adc85a6609b9ba1bf182e9219ae5a6564bcd20c
Mariana S <marianaletii@gmail.com>
2022-10-26 14:28:29 -0300

    Mudança no controller de galpoes em api

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -11,18 +11,17 @@ class Api::V1::WarehousesController < Api::V1::ApiController
   end
 
   def create
-    begin
-      warehouse_params = params.require(:warehouse).permit(:name, :code, :area, :useful_area, :city,
-                                                          :state, :address, :description, :cep)
-      warehouse = Warehouse.new(warehouse_params)
-      if warehouse.save
-        render status: 201, json: warehouse.as_json(except:  [:created_at, :updated_at])
-      else
-        render status: 412, json: { errors: warehouse.errors.full_messages }
-      end
-    rescue
-      render status: 500
+
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :area, :useful_area, :city,
+                                                        :state, :address, :description, :cep)
+    warehouse = Warehouse.new(warehouse_params)
+    if warehouse.save
+      return render status: 201, json: warehouse.as_json(except:  [:created_at, :updated_at])
+    else
+      return render status: 412, json: { errors: warehouse.errors.full_messages }
     end
+
+
   end
 
 
